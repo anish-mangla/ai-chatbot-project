bae68a2a92e35a2a2e2ab133ffdd0f1657260720
anish-mangla <143206049+anish-mangla@users.noreply.github.com>
2024-08-11 05:09:46 +0400

    created the index.js and index.css files

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// Render the App component into the root div
+ReactDOM.render(<App />, document.getElementById('root'));
